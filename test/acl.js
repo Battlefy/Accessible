53a3d1a49b617110529522c521666a8e60a58096
Shane Jonas <jonas.shane@gmail.com>
2014-01-29 14:23:12 -0800

    add more tests for error cases

--- test/acl.js
+++ test/acl.js
@@ -2,7 +2,7 @@ var acl = require('../');
 
 var controlList = {
   "/tournaments": {
-    getUserRole: function getUserRole (req, done){
+    getRole: function getUserRole (req, done){
       done(undefined, 'organizer');
     },
     get: ["admin", "organizer"],
@@ -41,7 +41,7 @@ describe('acl', function(){
     middleware(req, res, noop);
   });
 
-  it('can call getUserRole when no role is already defined', function(done){
+  it('can call getRole when no role is already defined', function(done){
     var controlList = {
       "/tournaments": {
         getRole: function getUserRole (req, callback){
@@ -68,6 +68,135 @@ describe('acl', function(){
     };
     var noop = function(err) {}
     middleware(req, res, noop);
+  });
+
+  it('can call next middleware when role is in METHOD array', function(done){
+    var controlList = {
+      "/tournaments": {
+        getRole: function getUserRole (req, callback){
+          callback(undefined, 'organizer');
+        },
+        get: ["admin", "organizer"],
+        put: ["admin", "organizer"],
+        post: ["admin", "organizer"],
+        delete: ["admin", "organizer"]
+      }
+    }
+    var middleware = acl(controlList);
+    var res = {
+      send: function() {},
+      end: function() {}
+    }
+    var req = {
+      url: '/tournaments',
+      method: 'GET',
+      route: { path: '/tournaments' },
+      user: {
+        _id: 1
+      }
+    };
+    var noop = function(err) {
+      if(err) { throw err }
+      done();
+    }
+    middleware(req, res, noop);
+  });
+
+
+  it('can send not authorized when getRole returns false', function(done){
+    var controlList = {
+      "/tournaments": {
+        getRole: function getUserRole (req, callback){
+          if(!req.user){
+            callback(undefined, false);
+          }
+          callback(undefined, 'organizer');
+        },
+        get: ["admin", "organizer"],
+        put: ["admin", "organizer"],
+        post: ["admin", "organizer"],
+        delete: ["admin", "organizer"]
+      }
+    }
+    var middleware = acl(controlList);
+    var res = {
+      send: function() {},
+      end: function() {}
+    }
+    var req = {
+      url: '/tournaments',
+      method: 'GET',
+      route: { path: '/tournaments' }
+    };
+    var noop = function(err) {
+      if(err) { throw err }
+      done();
+    }
+    middleware(req, res, noop);
+  });
+
+  it('can error when getRole doesnt return a string for a role', function(done){
+    var controlList = {
+      "/tournaments": {
+        getRole: function getUserRole (req, callback){
+          callback(undefined, {});
+        },
+        get: ["admin", "organizer"],
+        put: ["admin", "organizer"],
+        post: ["admin", "organizer"],
+        delete: ["admin", "organizer"]
+      }
+    }
+    var middleware = acl(controlList);
+    var res = {
+      send: function() {},
+      end: function() {}
+    }
+    var req = {
+      url: '/tournaments',
+      method: 'GET',
+      route: { path: '/tournaments' },
+      user: {
+        _id: 1
+      }
+    };
+    var noop = function(err) {
+      if(err) {
+        done();
+      }
+    }
+    middleware(req, res, noop);
+  });
+
+  it('can handle error cases when role returned is not in METHOD array', function(done){
+    var controlList = {
+      "/tournaments": {
+        getRole: function getUserRole (req, callback){
+          callback(undefined, 'organizer');
+        },
+        get: ["admin"]
+      }
+    }
+    var middleware = acl(controlList);
+    var res = {
+      send: function(statusCode, body) {
+        statusCode.should.equal(402);
+        body.error.should.equal('Not Authorized');
+        done();
+      },
+      end: function() {}
+    }
+    var req = {
+      url: '/tournaments',
+      method: 'GET',
+      route: { path: '/tournaments' },
+      user: {
+        _id: 1
+      }
+    };
+    var next = function(err) {
+    }
+    middleware(req, res, next);
   });
 
 });
